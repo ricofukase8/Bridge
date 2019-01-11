e70c17e76712c5be7ec61ba8cc538e81f1d605cb
鳥塚武 <>
2019-01-11 13:41:38 +0800

    signup画面

---
+++ signup/thinks.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>BRIDGE</title>
+    <link rel="stylesheet" type="text/css" href="signup.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+</head>
+<body>
+  <div id="top" class="jumbotron">
+    <div class="container">
+      <h1 class="title">BRIDGE</h1>
+        <p class="subtitle">-NexSeed-</p>
+    </div>
+  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2 thumbnail" style="border: 0px;">
+                <h2 class="text-center content_header">アカウント作成完了</h2>
+                <div class="text-center">
+                    <p>ユーザーアカウントの作成が完了しました。</p>
+                    <a href="signin.php" class="btn btn-primary next-step" id="next-btn">サインイン</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
+<!-- style="background-color: #00AFE5;" -->
