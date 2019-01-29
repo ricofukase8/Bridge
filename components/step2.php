82c167a26d5424a4d8355ec17f803dfe7a16f750
鳥塚武 <>
2019-01-29 21:03:34 +0800

    signupバリデーションの一部修正

--- components/step2.php
+++ components/step2.php
@@ -213,10 +213,10 @@
           </div>
           <div class="row">
             <div class="col-25">
-              <label for="job_offer_contents" class="step2_label disabled">求人情報</label>
+              <label for="job_offer_contents" class="disabled">求人情報</label>
             </div>
             <div class="col-75">
-              <textarea id="job_offer_contents" name="input_job_offer_contents" placeholder="Write job offer contents..." class="step2_form" disabled><?php echo isset($signin_user) ? $signin_user["offer_contents"] : ''; ?></textarea>
+              <textarea id="job_offer_contents" name="input_job_offer_contents" placeholder="Write job offer contents..." disabled><?php echo isset($signin_user) ? $signin_user["offer_contents"] : ''; ?></textarea>
             </div>
           </div>
           <div class="row">
