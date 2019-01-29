3b48c049787e0760d5f2dbd426554146c052b5b7
rico fukase <rico.fuakse8@gamil.com>
2019-01-29 23:18:54 +0800

    インデント整理

--- get_userdata.php
+++ get_userdata.php
@@ -1,7 +1,6 @@
 <?php
 require('dbconnect.php');
-
-// $name = $_GET['bridge']['name'];
+require('function.php');
 
 $sql = 'SELECT * FROM `advices`';
 $stmt = $dbh->prepare($sql);
@@ -9,19 +8,6 @@ $stmt->execute();
 
 $advices = $stmt->fetchAll();
 
-//edit画面だったら
 if(strpos($_SERVER['REQUEST_URI'], 'edit.php') !== false) {
-	$sql = "SELECT * FROM `users` LEFT JOIN `companies` ON `users` . `id` = `companies` . `user_id` LEFT JOIN `portfolios` ON `users` . `id` = `portfolios` . `user_id` WHERE `users` . `id` = ?";//DBからとる
-	$data = ["107"];
-	$stmt = $dbh->prepare($sql);
-    $stmt->execute($data);
-    $signin_user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // var_dump($signin_user); die();
-
-    // echo "<pre>";
-    // var_dump($signin_user);
-    // echo "</pre>";
-	// $user_name = 'ユーザーが入力した内容';
-	// $signin_user = ['2',3,4];
+	$signin_user = getSigninUser($dbh,$_SESSION['bridge']['id']);
 }
--- signin.php
+++ signin.php
@@ -3,18 +3,15 @@ session_start();
 require("dbconnect.php");
 require("function.php");
 
-
 $errors=[];
 
 if (!empty($_POST)) {
     $email = $_POST["input_email"];
     $password = $_POST["input_password"];
-    // var_dump($email);die();
     if ($email !== "" && $password !== "") {
         $signin_user = getUser($dbh,$email);
         if ($signin_user == false) {
             $errors["signin"]="failed";
-            // var_dump($errors["signin"]);
         }else{
             if (password_verify($password,$signin_user["password"])) {
                 $_SESSION["bridge"]["id"]=$signin_user["user_id"];
@@ -30,49 +27,46 @@ if (!empty($_POST)) {
     }
 }
 
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="utf-8">
-    <title>BRIDGE</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/signup/signup.css">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <meta charset="utf-8">
+  <title>BRIDGE</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/signup/signup.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 </head>
-<body >
+<body>
   <div id="top" class="jumbotron" style="background-image: url(assets/img/bridgemain.png);">
     <div class="container">
       <h1 class="title">BRIDGE</h1>
-        <p class="subtitle">-NexSeed-</p>
+      <p class="subtitle">-NexSeed-</p>
     </div>
   </div>
   <div class="container">
     <div class="row">
       <div class="col-xs-8 col-xs-offset-2 thumbnail" style="border: 0px;">
         <h2 class="text-center content_header">サインイン</h2>
-          <form method="POST" action="signin.php" enctype="multipart/form-data">
-            <div class="form-group">
-              <label for="email">メールアドレス</label>
-              <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
-              <?php if (isset($errors["signin"]) && $errors["signin"] == "blank"): ?>
-              <p class="text-danger">メールアドレスとパスワードを入力してください</p>
-              <?php endif ?>
-              <?php if (isset($errors["signin"]) && $errors["signin"] == "failed"): ?>
-              <p class="text-danger">ログインに失敗しました</p>
-              <?php endif ?>
-            </div>
-            <div class="form-group">
-                <label for="password">パスワード</label>
-                <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
-            </div>
-            <input type="submit" class="btn btn-primary next-step" id="next-btn" value="サインイン">
-            <span style="float: right; padding-top: 6px;">
-                <a href="main.php">戻る</a>
-            </span>
-          </form>
+        <form method="POST" action="signin.php" enctype="multipart/form-data">
+          <div class="form-group">
+            <label for="email">メールアドレス</label>
+            <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+                <?php if (isset($errors["signin"]) && $errors["signin"] == "blank"): ?>
+                <p class="text-danger">メールアドレスとパスワードを入力してください</p>
+                <?php endif ?>
+                <?php if (isset($errors["signin"]) && $errors["signin"] == "failed"): ?>
+                <p class="text-danger">ログインに失敗しました</p>
+                <?php endif ?>
+          </div>
+          <div class="form-group">
+            <label for="password">パスワード</label>
+            <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
+          </div>
+          <input type="submit" class="btn btn-primary next-step" id="next-btn" value="サインイン">
+          <span style="float: right; padding-top: 6px;">
+            <a href="main.php">戻る</a>
+          </span>
+        </form>
       </div>
     </div>
   </div>
