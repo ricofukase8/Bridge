a8a845bc5ca64feb0bbd0f2b89e85992329ef0de
鳥塚武 <>
2019-02-14 20:44:51 +0900

    editの修正

--- edit.php
+++ edit.php
@@ -336,8 +336,8 @@ while ($ex_advices = $stmt->fetch(PDO::FETCH_ASSOC)) {
   </div>
   </div>
 
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="assets/js/parsley.min.js"></script>
 <script src="assets/js/ja.js"></script>
 <script src="assets/js/ja.extra.js"></script>
