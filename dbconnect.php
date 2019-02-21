da4aa61b0672585b3927e49291af957beeb43c17
鳥塚武 <>
2019-02-21 22:05:11 +0900

    エラー修正

--- dbconnect.php
+++ dbconnect.php
@@ -6,10 +6,10 @@
     // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // $dbh->query('SET NAMES utf8');
 
-$db_name = getenv('DB_NAME');
-$host_name = getenv('HOST_NAME');
-$user = getenv('USER_NAME');
-$password = getenv('PASSWORD');
+$db_name = getenv(DB_NAME);
+$host_name = getenv(HOST_NAME);
+$user = getenv(USER_NAME);
+$password = getenv(PASSWORD);
 
 $dsn = 'mysql:dbname='.$db_name.';host=' . $host_name;
 $dbh = new PDO($dsn, $user, $password);
